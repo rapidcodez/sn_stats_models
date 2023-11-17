f5522b26eafdf4be1bbfd4df34593407409e30b6
Aravind-Ra <Aravind.rajendran@rci.rogers.com>
2023-11-17 12:49:05 -0500

    SNNGAP-1618, NCAAF Models

---
+++ ncaaf/game_data_web.go
@@ -0,0 +1,8 @@
+package ncaaf
+
+type GameDataWeb struct {
+	Details      DetailsWeb `json:"details"`
+	VisitingTeam TeamWeb    `json:"visiting_team"`
+	HomeTeam     TeamWeb    `json:"home_team"`
+	Quarters     []Quarter  `json:"quarters"`
+}
