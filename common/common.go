dd1c5de8c7a154a1ceadfb2cf70473a697f4b9e3
Sergey Chayko <sergey.chayko@rci.rogers.ca>
2024-05-29 11:42:16 -0400

    NHLWEB-17632, remove duplicated season type

--- common/common.go
+++ common/common.go
@@ -35,7 +35,6 @@ type RecentGames struct {
 	VisitingTeamID    string `json:"visiting_team_id,omitempty"`
 	HomeTeam          string `json:"home_team,omitempty"`
 	HomeTeamID        string `json:"home_team_id,omitempty"`
-	SeasonType        int    `json:"season_type,omitempty"`
 }
 
 type Story struct {
